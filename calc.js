643221e9b87b12d117419260e3c0a53fcf785818
Eugene Soo <eugene@soo.sg>
2017-11-21 23:07:54 -0800

    refactor calculator code and restyle

--- calc.js
+++ calc.js
@@ -1,136 +1,110 @@
-document.addEventListener('DOMContentLoaded', (e) => {
-  let currentValue;
-  let previousValue;
-  let currentOperation;
+let currentValue;
+let previousValue;
+let currentOperation;
 
-  const buttons = document.getElementsByTagName('button');
+const resetValues = () => {
+  currentValue = 0;
+  previousValue = undefined;
+  currentOperation = undefined;
+	document.getElementById('numbers').value = currentValue;
+  document.getElementById('operation').value = '';
+};
 
-  const resetValues = () => {
-    document.getElementById('numbers').value = 0;
-    currentValue = 0;
-    previousValue = undefined;
-    currentOperation = undefined;
-  };
-
-  const setCurrentValue = (value = 0) => {
+const setCurrentValue = (value = 0, isResult) => {
+	if (currentValue === 0 || isResult) {
     document.getElementById('numbers').value = value;
-    currentValue = value;
-  };
-
-  const setPreviousvalue = (value) => {
-    previousValue = value;
-    currentValue = undefined;
-  };
-
-  const setOperation = (value) => {
-    currentOperation = value;
-    document.getElementById('operation').value = currentOperation;  
-  };
-
-  const computeNums = (operation, a, b) => {
-    switch (operation) {
-      case 'x':
-        return a * b;
-        break;
-      case '-':
-        return a - b;
-        break;
-      case '+':
-        return a + b;
-        break;
-      case '/':
-        return a / b;
-        break;
-      default:
-        break;
-    }
+  } else {
+	  document.getElementById('numbers').value += value;
   }
+  	currentValue = parseInt(document.getElementById('numbers').value, 10);
+};
 
-  const numClick = (event) => {
-    switch (event.target.innerHTML) {
-      case '0':
-        if (!previousValue) {
-          setCurrentValue();
-        } else {
-          setCurrentValue(computeNums(currentOperation, previousValue, 0));
-        }
-        break;
-      case '1':
-        if (!previousValue) {
-          setCurrentValue(1);
-        } else {
-          setCurrentValue(computeNums(currentOperation, previousValue, 1));
-        }
-        break;
-      case '2':
-        if (!previousValue) {
-          setCurrentValue(2);
-        } else {
-          setCurrentValue(computeNums(currentOperation, previousValue, 2));
-        }
-        break;
-      case '3':
-        if (!previousValue) {
-          setCurrentValue(3);
-        } else {
-          setCurrentValue(computeNums(currentOperation, previousValue, 3));
-        }
-        break;
-      case '4':
-        if (!previousValue) {
-          setCurrentValue(4);
-        } else {
-          setCurrentValue(computeNums(currentOperation, previousValue, 4));
-        }
-        break;
-      case '5':
-        if (!previousValue) {
-          setCurrentValue(5);
-        } else {
-          setCurrentValue(computeNums(currentOperation, previousValue, 5));
-        }
-        break;
-      case '6':
-        if (!previousValue) {
-          setCurrentValue(6);
-        } else {
-          setCurrentValue(computeNums(currentOperation, previousValue, 6));
-        }
-        break;
-      case '7':
-        if (!previousValue) {
-          setCurrentValue(7);
-        } else {
-          setCurrentValue(computeNums(currentOperation, previousValue, 7));
-        }
-        break;
-      case '8':
-        if (!previousValue) {
-          setCurrentValue(8);
-        } else {
-          setCurrentValue(computeNums(currentOperation, previousValue, 8));
-        }
-        break;
-      case '9':
-        if (!previousValue) {
-          setCurrentValue(9);
-        } else {
-          setCurrentValue(computeNums(currentOperation, previousValue, 9));
-        }
-        break;
-      case 'AC':
-        resetValues();
-      default:
-        if (currentValue) {
-          setPreviousvalue(currentValue);
-        };
-        setOperation(event.target.innerHTML);
-        break;
-    }
-  }
+const setPreviousvalue = (value) => {
+	previousValue = value;
+  currentValue = 0;
+};
 
-  setCurrentValue();
-  for (let i = 0; i < buttons.length; i += 1) {
-    buttons[i].addEventListener('click', numClick);
+const setOperation = (value) => {
+	currentOperation = value;
+	document.getElementById('operation').value = currentOperation;  
+};
+
+const computeNums = (operation, a, b) => {
+	switch (operation) {
+  	case 'X':
+    	return a * b;
+    	break;
+    case '-':
+    	return a - b;
+      break;
+    case '+':
+    	console.log(a + b);
+    	return a + b;
+      break;
+    case '/':
+    	return a / b;
+      break;
+    default:
+    	break;
   }
-});
+}
+
+const numClick = (event) => {
+	switch (event.target.innerHTML) {
+  	case '0':
+      setCurrentValue();
+      break;
+  	case '1':
+      setCurrentValue(1);
+      break;
+  	case '2':
+			setCurrentValue(2);
+      break;
+  	case '3':
+			setCurrentValue(3);
+      break;
+  	case '4':
+    	setCurrentValue(4);
+      break;
+  	case '5':
+    	setCurrentValue(5);
+      break;
+  	case '6':
+    	setCurrentValue(6);
+      break;
+  	case '7':
+    	setCurrentValue(7);
+      break;
+  	case '8':
+    	setCurrentValue(8);
+      break;
+  	case '9':
+    	setCurrentValue(9);
+      break;
+    case 'AC':
+    	resetValues();
+      break;
+    case '=':
+      setCurrentValue(computeNums(currentOperation, previousValue, currentValue), true);
+      currentOperation = undefined;
+			document.getElementById('operation').value = '=';  
+    	break;
+    default:
+    	if (!currentOperation) {
+      	setPreviousvalue(currentValue);
+      	setOperation(event.target.innerHTML); 
+      } else if (!currentValue) {
+      	
+      } else {
+      	setOperation(event.target.innerHTML);       
+      }
+    	break;
+  }
+}
+
+const buttons = document.getElementsByTagName('button');
+
+setCurrentValue();
+for (let i = 0; i < buttons.length; i += 1) {
+	buttons[i].addEventListener('click', numClick);
+}
